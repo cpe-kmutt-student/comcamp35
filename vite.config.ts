e3848df98eb2b3f83414ff1902ab12ed19ccb50a
Mirailisc <mirailisclm@gmail.com>
2024-01-15 19:54:08 +0700

    init necessary packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
+import * as path from 'path'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+const ENV_PREFIX = 'APP_'
+
+export default defineConfig(({ mode }) => {
+  process.env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: '/',
+    plugins: [react(), eslint(), tsconfigPaths()],
+    resolve: {
+      alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    },
+    envPrefix: ENV_PREFIX,
+    server: {
+      port: 3000,
+    },
+    preview: {
+      host: '0.0.0.0',
+      port: 8000,
+    },
+  }
 })
