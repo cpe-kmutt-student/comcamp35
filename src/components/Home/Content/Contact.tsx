80e675a63845a27209444c91a34ac868d29db74f
Patihan Prasongphol <patihan70@gmail.com>
2024-01-29 20:56:37 +0700

    AddOtherContentSection

---
+++ src/components/Home/Content/Contact.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Contact: React.FC = () => {
+  return (
+    <div>
+      <h2>• ช่องทางการติดต่อ</h2>
+      <p>
+        • Address ภาควิชาวิศวกรรมคอมพิวเตอร์ คณะวิศวกรรมศาสตร์มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรีชั้น 10
+        อาคารวิศววัฒนะ เลขที่ 126 ถ.ประชาอุทิศ แขวงบางมด เขตทุ่งครุ กรุงเทพฯ 10140
+        <br></br>• Contect พี่ฟีฟ่า : 080 493 5928 พี่หมูเล็ก : 093 583 1518 พี่น้ำขิง : 061 506 6249 พี่พลอย : 097 940
+        7172
+        <br></br>• Social Media Facebook: Comcamp KMUTT Instagram: comcamp.kmutt Tiktok: comcamp.kmutt
+      </p>
+    </div>
+  )
+}
+
+export default Contact
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -3,6 +3,13 @@ import ShortStory from './Content/Shortstory'
 import Invitation from './Content/Invitation'
 import About from './Content/About'
 import CampHighlight from './Content/Camphighlight'
+import Condition from './Content/Condition'
+import Timeline from './Content/Timeline'
+import ReceivingNum from './Content/ReceivingNum'
+import Location from './Content/Location'
+import FAQ from './Content/FAQ'
+import Sponsor from './Content/Sponsor'
+import Contact from './Content/Contact'
 
 const Content: React.FC = () => {
   return (
@@ -11,6 +18,13 @@ const Content: React.FC = () => {
       <About />
       <ShortStory />
       <CampHighlight />
+      <Condition />
+      <Timeline />
+      <ReceivingNum />
+      <Location />
+      <FAQ />
+      <Sponsor />
+      <Contact />
     </div>
   )
 }
