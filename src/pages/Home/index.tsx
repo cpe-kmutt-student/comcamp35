cc3da7c3c404609b3018c80a3300db67bd29f4e1
Patihan Prasongphol <patihan70@gmail.com>
2024-01-29 09:55:13 +0700

    initialContent

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,14 @@
 import { useAuth } from 'src/context/auth'
-
+import Content from 'src/components/Home'
 const Home: React.FC = (): JSX.Element => {
   const { auth } = useAuth()
 
-  return <div>{auth.is_authenticated && <h2>Welcome, {auth.firstName + ' ' + auth.lastName}</h2>}</div>
+  return (
+    <>
+      <div>{auth.is_authenticated && <h2>Welcome, {auth.firstName + ' ' + auth.lastName}</h2>}</div>
+      <Content />
+    </>
+  )
 }
 
 export default Home
