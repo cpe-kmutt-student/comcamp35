37540a8511e5b5feedaeb5e896635fef9354d0eb
Mirailisc <mirailisclm@gmail.com>
2024-01-31 00:34:23 +0700

    fix: useless css properties

--- src/components/Home/Content/Hero.tsx
+++ src/components/Home/Content/Hero.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import styles from './Hero.module.scss'
+import RegisterButton from 'src/components/RegisterButton'
+import Logo from 'src/assets/logo.webp'
+import { useAuth } from 'src/context/auth'
 
 const Hero: React.FC = () => {
+  const { auth } = useAuth()
+
   return (
     <div className={styles.hero}>
-      <img className={styles.logo} alt="Logo" src="src\assets\logo.webp" />
-      <div className={styles.textWrapper}>7-15 April 2024</div>
+      <img className={styles.logo} alt="logo" src={Logo} />
+      <h1 className={styles.campDate}>7-15 April 2024</h1>
       <p className={styles.content}>
         เมื่อโลกโปรเเกรมมอนกำลังจะล่มสลาย<br></br>
         เหล่าหัวหน้ากิลด์จึงได้ทำการรวบรวมบรรดาเด็กน้อยผู้มีพรสวรรค์ที่ซ่อนเร้นเพื่อทำการฝึกฝนและปลุกทักษะที่หลับใหลให้ตื่นขึ้น
-        ! เพื่อรวมพลังกันกอบกู้โลกใบนี้ให้รอดพ้นจากเหล่าวายร้าย !!
+        ! <br />
+        เพื่อรวมพลังกันกอบกู้โลกใบนี้ให้รอดพ้นจากเหล่าวายร้าย !!
       </p>
-      <button className={styles.button}>
-        <div className={styles.textWrapper2}>ลงทะเบียน</div>
-      </button>
-      <br></br>
+      {!auth.is_authenticated && <RegisterButton active>ลงทะเบียน</RegisterButton>}
     </div>
   )
 }
--- src/components/Home/Content/ReceivingNum.tsx
+++ src/components/Home/Content/ReceivingNum.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+// TODO: Might consider merge this with other component
+
 const ReceivingNum: React.FC = () => {
   return (
     <div>
-      <h2>• จำนวนที่รับ : 80 คน</h2>
+      <h2>จำนวนที่รับ : 80 คน</h2>
       <br></br>
     </div>
   )
