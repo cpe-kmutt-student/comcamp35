d91f2177c050337117b8a5f379ebb435f631ed0e
SuperSonicBoomEX <ben.peerawish@gmail.com>
2024-02-12 23:27:24 +0700

    <added contact template (first draft)>

--- src/components/Home/Content/Contact.tsx
+++ src/components/Home/Content/Contact.tsx
@@ -1,18 +1,51 @@
 import React from 'react'
+import styles from './contact.module.scss'
+import { Heading } from '@radix-ui/themes'
 
 const Contact: React.FC = () => {
   return (
     <div>
-      <h2>ช่องทางการติดต่อ</h2>
-      <p>
-        Address ภาควิชาวิศวกรรมคอมพิวเตอร์ คณะวิศวกรรมศาสตร์มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรีชั้น 10 อาคารวิศววัฒนะ
-        เลขที่ 126 ถ.ประชาอุทิศ แขวงบางมด เขตทุ่งครุ กรุงเทพฯ 10140
-        <br></br>• Contect พี่ฟีฟ่า : 080 493 5928 พี่หมูเล็ก : 093 583 1518 พี่น้ำขิง : 061 506 6249 พี่พลอย : 097 940
-        7172
-        <br></br>• Social Media Facebook: ComCamp KMUTT Instagram: comcamp.kmutt Tiktok: comcamp.kmutt
-      </p>
+      <Heading size={{ initial: '8', md: '9' }} className="title contacttitle" align="center">
+        CONTACT
+      </Heading>
+      <div className={styles.container}>
+        <div className={styles.leftblock}>
+          <ul>
+            <p>พี่ฟีฟ่า</p>
+            <p>080 493 5928</p>
+            <p>yanakorn.tang@mail.kmutt.ac.th</p>
+            <ol>พี่หมูเล็ก</ol>
+            <p>093 583 1518</p>
+            <p>pimpisa.sods@mail.kmutt.ac.th</p>
+            <ol>พี่หมูเล็ก</ol>
+            <p> 061 506 6249</p>
+            <p>wipada.kasi@mail.kmutt.ac.th</p>
+            <ol>พี่พลอย</ol>
+            <p>097 940 7172</p>
+            <p>chatchalita.kaew@mail.kmutt.ac.th</p>
+          </ul>
+        </div>
+        <div className={styles.column}>
+          <div className={styles.topblock}>
+            <h3>Social Media</h3>
+            <p>Facebook: /yourFacebook</p>
+            <p>Instagram: @yourInstagram</p>
+            <p>Tiktok: @yourTiktok</p>
+          </div>
+          <div className={styles.bottomblock}>
+            <h1>Address</h1>
+            <p>ภาควิชาวิศวกรรมคอมพิวเตอร์ คณะวิศวกรรมศาสตร์ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี</p>
+            <p>ชั้น 10 อาคารวิศววัฒนะ เลขที่ 126 ถ.ประชาอุทิศ แขวงบางมด เขตทุ่งครุ กรุงเทพฯ 10140</p>
+            <div className={styles.addressbutton}>
+              <a href="https://maps.app.goo.gl/TBaXUSg1Czpv1WwR9" rel="noreferrer" target="_blank">
+                ดูในแผนที่
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    // responsive, format prettier
   )
 }
-
 export default Contact
