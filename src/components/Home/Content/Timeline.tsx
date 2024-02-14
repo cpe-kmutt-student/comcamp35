dc8b14413de49f75bb9cfb8ff3c848de145b3cd4
PUN <punnawat.mungkalarungsi@gmail.com>
2024-02-14 15:02:45 +0700

    Refactor timeline component and add dynamic rendering of camp conditions

--- src/components/Home/Content/Timeline.tsx
+++ src/components/Home/Content/Timeline.tsx
@@ -1,18 +1,51 @@
 import React from 'react'
+import { Flex, Heading } from '@radix-ui/themes'
+import styles from './timeline.module.scss'
+import timeline_logo2 from 'src/assets/Aquack.svg.png'
+import timeline_logo1 from 'src/assets/Candy-Gray.svg.png'
+import timeline_logo3 from 'src/assets/Candy-Pink.svg.png'
+import timeline_logo4 from 'src/assets/Candy-Red.svg.png'
+
+const TimelineContents: { date: string; event: String; image: string }[] = [
+  {
+    date: '1 - 10 มีนาคม 2567',
+    event: 'รับสมัคร',
+    image: timeline_logo1,
+  },
+  {
+    date: '16 มีนาคม 2567',
+    event: 'ประกาศผล',
+    image: timeline_logo2,
+  },
+  {
+    date: '16 - 18 มีนาคม 2567',
+    event: 'ยืนยันสิทธ์',
+    image: timeline_logo3,
+  },
+  {
+    date: '7 - 11 เมษายน 2567',
+    event: 'วันค่าย',
+    image: timeline_logo4,
+  },
+]
 
 const Timeline: React.FC = () => {
+  const renderCampCondition = TimelineContents.map((item, i) => (
+    <ol key={i}>
+      <img src={item.image} alt="" style={{ width: '120px', height: '120px', objectFit: 'cover' }} />
+      <p>{item.date}</p>
+      <h2>{item.event}</h2>
+    </ol>
+  ))
   return (
-    <div>
-      <h2>timeline</h2>
-      <p>
-        - เปิดรับสมัคร: 1 - 10 มีนาคม 2567
-        <br></br>- ประกาศผล: 16 มีนาคม 2567
-        <br></br>- สามารถเข้าพักได้ในระยะเวลาและสถานที่ที่กำหนดตลอดโครงการ
-        <br></br>- ยืนยันสิทธ์: 16 - 18 มีนาคม 2567
-        <br></br>- วันค่าย 7 - 11 เมษายน 2567
-      </p>
-      <br></br>
-    </div>
+    <Flex direction="column" justify="center" align="center" className={styles.timeline}>
+      <Heading size={{ initial: '8', md: '9' }} className="title" align="center">
+        timeline
+      </Heading>
+      <div className="timeline_layout">
+        <div className={styles.item_list_timeline}>{renderCampCondition}</div>
+      </div>
+    </Flex>
   )
 }
 
