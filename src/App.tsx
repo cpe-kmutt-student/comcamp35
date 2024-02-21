5d2714e8141ff434e6f1a4dfdeb238249a9a318e
Phubordin Poolnai <mirailisclm@gmail.com>
2024-02-21 12:22:06 +0700

    fix: google analytics env

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,9 @@ import ReactGA from 'react-ga4'
 const App: React.FC = (): JSX.Element => {
   const location = useLocation()
 
-  ReactGA.initialize(import.meta.env.VITE_GA_MEASUREMENT_ID)
+  if (import.meta.env.VITE_GA_MEASUREMENT_ID) {
+    ReactGA.initialize(import.meta.env.VITE_GA_MEASUREMENT_ID)
+  }
 
   const navbarFilter = () => {
     switch (location.pathname) {
