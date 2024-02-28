2a9fd5b30e8ac54bd6d239dc95f113582eb56701
Mirailisc <mirailisclm@gmail.com>
2024-02-28 22:11:09 +0700

    chore: change document url

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,4 +1,4 @@
 export const UPLOAD_ENDPOINT = 'https://up.m1r.ai/upload'
-export const PDF_QUESTION = 'https://m1r.ai/9/1s0gn.mp4'
-export const WORD_QUESTION = 'https://m1r.ai/9/1s0gn.mp4'
-export const PARENTS_APPROVEMENT = 'https://api-s3.kronos.moe/comcamp/approval.pdf'
+export const PDF_QUESTION = 'https://m1r.ai/9/ypwi9.pdf'
+export const WORD_QUESTION = 'https://m1r.ai/9/nltbp.docx'
+export const PARENTS_APPROVEMENT = 'https://m1r.ai/9/tryuw.pdf'
