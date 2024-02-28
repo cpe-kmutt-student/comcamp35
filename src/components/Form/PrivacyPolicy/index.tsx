4c8dbfa3e3df14edac6f52f16374f6d9d4e46f9f
Jedsadaporn pannok <tikpop555@gmail.com>
2024-02-28 20:12:11 +0700

    Fix : PrivacyPolicy

--- src/components/Form/PrivacyPolicy/index.tsx
+++ src/components/Form/PrivacyPolicy/index.tsx
@@ -1,6 +1,4 @@
-import PropTypes from 'prop-types' // Import PropTypes
-
-const PrivacyPolicy = ({ onAccept }: { onAccept: () => void }) => {
+const PrivacyPolicy = () => {
   return (
     <div>
       <h1>นโยบายความเป็นส่วนตัว</h1>
@@ -8,15 +6,8 @@ const PrivacyPolicy = ({ onAccept }: { onAccept: () => void }) => {
         เราใส่ใจความเป็นส่วนตัวของคุณและการปกป้องข้อมูลของคุณอย่างยิ่ง
         โปรดอ่านนโยบายความเป็นส่วนตัวนี้เพื่อทราบถึงวิธีที่เราใช้และปกป้องข้อมูลของคุณ
       </p>
-      {/* เพิ่มปุ่มยอมรับ */}
-      <button onClick={onAccept}>ยอมรับนโยบายส่วนบุคคล</button>
     </div>
   )
-}
-
-// ระบุ PropTypes ของ onAccept เพื่อตรวจสอบชนิดของ props ที่คาดหวัง
-PrivacyPolicy.propTypes = {
-  onAccept: PropTypes.func.isRequired, // ระบุให้ onAccept เป็นฟังก์ชันที่ต้องส่งเข้ามาใน props และเป็น required
 }
 
 export default PrivacyPolicy
