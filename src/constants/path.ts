fcace249e0966fee5d7456ce49eab6dad3853330
Mirailisc <mirailisclm@gmail.com>
2024-02-29 02:37:28 +0700

    fix: filename

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,4 +1,4 @@
 export const UPLOAD_ENDPOINT = 'https://up.m1r.ai/upload'
 export const PDF_QUESTION = 'https://m1r.ai/9/o4vmt.pdf'
 export const WORD_QUESTION = 'https://m1r.ai/9/jckab.docx'
-export const PARENTS_APPROVEMENT = 'https://m1r.ai/9/tryuw.pdf'
+export const PARENTS_APPROVEMENT = 'https://api-s3.kronos.moe/comcamp/comcamp35-parents-permission-letter.pdf'
