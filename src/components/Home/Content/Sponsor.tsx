dcfb6a6fce9b527a37d0ff6995a9ac0fc2846a91
PUN <punnawat.mungkalarungsi@gmail.com>
2024-02-29 23:47:49 +0700

    Update sponsor logos and adjust sizes

--- src/components/Home/Content/Sponsor.tsx
+++ src/components/Home/Content/Sponsor.tsx
@@ -4,6 +4,9 @@ import styles from './sponsor.module.scss'
 import LDA from 'src/assets/lda_logo.webp'
 import ThaiData from 'src/assets/thaidata_logo.webp'
 import Advice from 'src/assets/advice_logo.webp'
+import Cpe from 'src/assets/cpe_logo.svg'
+import Kmutt from 'src/assets/kmutt_logo.svg'
+// import Applicad from 'src/assets/applicad_logo.webp'
 
 interface ISponsor {
   img: string
@@ -23,20 +26,27 @@ const Sponsors: ISponsor[] = [
     img: ThaiData,
     level: 'Gold',
   },
+  // {
+  //   img: Applicad,
+  //   level: 'Gold',
+  // },
 ]
 
 const Sponsor: React.FC = () => {
   const renderSponsor = (rank: string) => {
     return Sponsors.filter((sponsor) => sponsor.level === rank).map((sponsor, i) => {
-      let size_img = '60px' // Default size for all ranks
+      let size_img_h = '60px' // Default size for all ranks
+      let size_img_w = 'auto'
       if (rank === 'Diamond') {
-        size_img = '160px' // Adjust size for Diamond rank
+        size_img_h = '160px' // Adjust size for Diamond rank
       } else if (rank === 'Platinum') {
-        size_img = '100px' // Adjust size for Platinum rank
+        size_img_h = '100px' // Adjust size for Platinum rank
+      } else if (rank === 'Gold') {
+        size_img_w = '200px' // Adjust size for Gold rank
       }
       return (
         <div key={i} className={styles.logo}>
-          <img src={sponsor.img} style={{ height: size_img, width: 'auto' }} alt={`sponsor-${i}`} />
+          <img src={sponsor.img} style={{ height: size_img_h, width: size_img_w }} alt={`sponsor-${i}`} />
         </div>
       )
     })
@@ -44,18 +54,24 @@ const Sponsor: React.FC = () => {
 
   return (
     <Flex direction="column" justify="center" align="center" className={styles.sponsor} mt={{ initial: '9', md: '0' }}>
+      <div className={styles.bg_logo_top}>
+        <img src={Kmutt} alt="kmutt" style={{ height: '140px' }} />
+        <img src={Cpe} alt="cpe" style={{ height: '120px' }} />
+      </div>
       <Heading size={{ initial: '8', md: '9' }} className="title">
         SPONSOR
       </Heading>
-      <Grid columns={{ initial: '1', sm: '1', md: '1' }} className={styles.contents}>
-        {renderSponsor('Diamond')}
-      </Grid>
-      <Grid columns={{ initial: '1', sm: '1', md: '1' }} className={styles.contents}>
-        {renderSponsor('Platinum')}
-      </Grid>
-      <Grid columns={{ initial: '1', sm: '1', md: '1' }} className={styles.contents}>
-        {renderSponsor('Gold')}
-      </Grid>
+      <div style={{ width: '80%' }}>
+        <Grid columns={{ initial: '1', sm: '1', md: '1' }} className={styles.contents}>
+          {renderSponsor('Diamond')}
+        </Grid>
+        <Grid columns={{ initial: '1', sm: '1', md: '1' }} className={styles.contents}>
+          {renderSponsor('Platinum')}
+        </Grid>
+        <Grid columns={{ initial: '1', sm: '1', md: '1' }} className={styles.contents}>
+          {renderSponsor('Gold')}
+        </Grid>
+      </div>
     </Flex>
   )
 }
