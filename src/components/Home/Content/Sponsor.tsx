372a1cf26c003c7bb51e008fe49b524a473a96d4
PUN <punnawat.mungkalarungsi@gmail.com>
2024-02-29 14:34:59 +0700

    Refactor sponsor component to dynamically render sponsors based on their rank

--- src/components/Home/Content/Sponsor.tsx
+++ src/components/Home/Content/Sponsor.tsx
@@ -3,50 +3,58 @@ import React from 'react'
 import styles from './sponsor.module.scss'
 import LDA from 'src/assets/lda_logo.webp'
 import ThaiData from 'src/assets/thaidata_logo.webp'
-import CPE from 'src/assets/cpe_logo.webp'
 import Advice from 'src/assets/advice_logo.webp'
 
 interface ISponsor {
   img: string
+  level?: string
 }
 
 const Sponsors: ISponsor[] = [
   {
     img: Advice,
+    level: 'Diamond',
   },
   {
     img: LDA,
+    level: 'Platinum',
   },
   {
     img: ThaiData,
-  },
-  {
-    img: CPE,
+    level: 'Gold',
   },
 ]
 
 const Sponsor: React.FC = () => {
-  const renderSponsor = Sponsors.map((sponsor: ISponsor, i: number) => {
-    return (
-      <div key={i} className={styles.logo}>
-        <img src={sponsor.img} alt="sponsor" loading="lazy" />
-      </div>
-    )
-  })
+  const renderSponsor = (rank: string) => {
+    return Sponsors.filter((sponsor) => sponsor.level === rank).map((sponsor, i) => {
+      let size_img = '60px' // Default size for all ranks
+      if (rank === 'Diamond') {
+        size_img = '160px' // Adjust size for Diamond rank
+      } else if (rank === 'Platinum') {
+        size_img = '100px' // Adjust size for Platinum rank
+      }
+      return (
+        <div key={i} className={styles.logo}>
+          <img src={sponsor.img} style={{ height: size_img, width: 'auto' }} alt={`sponsor-${i}`} />
+        </div>
+      )
+    })
+  }
 
   return (
     <Flex direction="column" justify="center" align="center" className={styles.sponsor} mt={{ initial: '9', md: '0' }}>
       <Heading size={{ initial: '8', md: '9' }} className="title">
         SPONSOR
       </Heading>
-      <Grid
-        columns={{ initial: '1', sm: '2', md: '4' }}
-        width="auto"
-        justify="center"
-        align="center"
-        className={styles.contents}
-      >
-        {renderSponsor}
+      <Grid columns={{ initial: '1', sm: '1', md: '1' }} className={styles.contents}>
+        {renderSponsor('Diamond')}
+      </Grid>
+      <Grid columns={{ initial: '1', sm: '1', md: '1' }} className={styles.contents}>
+        {renderSponsor('Platinum')}
+      </Grid>
+      <Grid columns={{ initial: '1', sm: '1', md: '1' }} className={styles.contents}>
+        {renderSponsor('Gold')}
       </Grid>
     </Flex>
   )
