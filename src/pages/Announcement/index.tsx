c7d5a31fd43419148102332cd0582c301d431eb6
Patihan Prasongphol <patihan70@gmail.com>
2024-03-15 13:55:10 +0700

    a bit of styling and split content to new file

--- src/pages/Announcement/index.tsx
+++ src/pages/Announcement/index.tsx
@@ -3,15 +3,30 @@ import { Flex, Heading } from '@radix-ui/themes'
 import styles from './index.module.scss'
 import { Link } from 'react-router-dom'
 import { BASE_PATH } from 'src/constants/router'
+import { ResultList } from './resultlist'
 
-const ResultsAnnouncement: React.FC = () => {
+type Props = {
+  active?: boolean
+}
+
+const ResultsAnnouncement: React.FC<Props> = (active) => {
+  const renderresult = ResultList.map((item, i) => (
+    <div key={i} className={styles.content}>
+      <div>
+        {item.Firstname}
+        {` `}
+        {item.Lastname}
+      </div>
+    </div>
+  ))
   return (
-    <Flex direction="column" justify="center" align="center" className={styles.faq}>
+    <Flex direction="column" justify="center" align="center" className={styles.announcement}>
       <Heading size={{ initial: '8', md: '9' }} className="title" align="center">
         ประกาศผลการคัดเลือก ComCamp 35
       </Heading>
-      <div className={styles.content}>อ้ายยังบ่ใส่ข้อมูล</div>
-      <Link to={BASE_PATH} className={styles.back}>
+      <hr />
+      <div className={styles.content}>{renderresult}</div>
+      <Link to={BASE_PATH} className={active ? styles.buttonActive : styles.button}>
         ย้อนกลับ
       </Link>
     </Flex>
---
+++ src/pages/Announcement/resultlist.ts
@@ -0,0 +1,22 @@
+export const ResultList: { Firstname: string; Lastname: string }[] = [
+  {
+    Firstname: 'ไม่บอก',
+    Lastname: 'อะไรไม่รู้',
+  },
+  {
+    Firstname: 'สมชาย',
+    Lastname: 'หมายปอง',
+  },
+  {
+    Firstname: 'ไอ้ติ๊ก',
+    Lastname: 'กูขี้เกียจ กูทำแบบนี้ได้มั้ย',
+  },
+  {
+    Firstname: 'ok',
+    Lastname: 'ok',
+  },
+  {
+    Firstname: 'ร้อนเกินชั้น5',
+    Lastname: 'กูอยากย้ายหอ',
+  },
+]
