411f658ff3773512a8a30f9814fb9d2382376fdf
Mirailisc <mirailisclm@gmail.com>
2024-03-19 01:00:10 +0700

    fix: slow performance due to the context

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.scss'
 import { BrowserRouter } from 'react-router-dom'
-import AuthProvider from './context/auth-context.tsx'
+// import AuthProvider from './context/auth-context.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <AuthProvider>
+    {/* <AuthProvider>
       <App />
-    </AuthProvider>
+    </AuthProvider> */}
+    <App />
   </BrowserRouter>,
 )
